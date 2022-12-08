18134b103fd6e2d986a529aeac4864426bed9964
Hanqi Yang <68807311+grimmmar@users.noreply.github.com>
2022-12-08 15:06:09 +0100

    Add files via upload

--- federated_main.py
+++ federated_main.py
@@ -123,7 +123,6 @@ if __name__ == '__main__':
     # Test inference after completion of training
     test_acc, test_loss = test_inference(args, global_model, test_dataset)
 
-
     print(f' \n Results after {args.epochs} global rounds of training:')
     print("|---- Avg Train Accuracy: {:.2f}%".format(100 * train_accuracy[-1]))
     print("|---- Test Accuracy: {:.2f}%".format(100 * test_acc))
